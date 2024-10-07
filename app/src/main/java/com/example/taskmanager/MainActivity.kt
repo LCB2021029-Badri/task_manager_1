e1d23fe1b1284b09b77f6fb412992aeea400485d
badri-akkala <lcb2021029@iiitl.ac.in>
2024-10-07 19:36:51 +0530

    final working app (no testing)

--- app/src/main/java/com/example/taskmanager/MainActivity.kt
+++ app/src/main/java/com/example/taskmanager/MainActivity.kt
@@ -1,16 +1,35 @@
 package com.example.taskmanager
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
 import com.example.taskmanager.ui.list.TaskFragment
 
 class MainActivity : AppCompatActivity() {
+
+    // check which theme of app in shared preference
+    private lateinit var sharedPreferences: SharedPreferences
+    private val PREF_NAME = "themePref"
+    private val KEY_THEME = "isDarkMode"
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+        sharedPreferences = getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE)
+        val isDarkMode = sharedPreferences.getBoolean(KEY_THEME, false)
+
+        // Set the theme based on the shared preference
+        if (isDarkMode) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+        }
+
+        super.onCreate(savedInstanceState) // Call this after setting the theme
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
@@ -18,7 +37,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // badri's code
+
         if (savedInstanceState == null) {
             loadFragment(TaskFragment())
         }
